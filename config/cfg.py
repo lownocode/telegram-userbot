2a2c621086f59057e1c034743cc01ee04da021a3
Localhostov <localhostov@gmail.com>
2023-02-05 03:40:11 +0300

    Create cfg.py

---
+++ config/cfg.py
@@ -0,0 +1,10 @@
+# https://my.telegram.org/apps
+
+API_ID = 123
+API_HASH = "qwerty"
+USER_ID = 123  # ur user id in Telegram
+
+# TTS config:
+
+language = "ru"  # for example: "ru", "en", "de", "ua"
+tts_speed = False  # True - slow, False - fast
