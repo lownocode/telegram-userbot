403112024be6d5e6d5984e71db92268911920fc9
whateverwhoami <ladnododik@gmail.com>
2023-02-05 05:20:53 +0000

    some changes

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ from pyrogram.handlers import MessageHandler
 from commands.TTS import send_tts_message
 from config import API_ID, API_HASH
 
-app = Client("tts_bot", API_ID, API_HASH)
+app = Client(
+    "user_bot",
+    API_ID,
+    API_HASH,
+    hide_password=True
+)
 
 app.add_handler(MessageHandler(send_tts_message))
 
